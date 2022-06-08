cf0f74363a037b5134eb18ad33736407fb714371
abdullahboshir <https://github.com/abdullahboshir/t-shirt-react-router>
2022-06-08 11:13:37 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWO5Z5Y-46UB-0-k-QrzqSaHHjV3pXlHc",
+  authDomain: "router-firebase-integrat-17580.firebaseapp.com",
+  projectId: "router-firebase-integrat-17580",
+  storageBucket: "router-firebase-integrat-17580.appspot.com",
+  messagingSenderId: "1053496596212",
+  appId: "1:1053496596212:web:f0a15b688759ddf4e3b0a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
