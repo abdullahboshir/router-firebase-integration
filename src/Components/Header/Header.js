92e791651a42f3cf0b255f0a387e24ac41b6c4e1
abdullahboshir <https://github.com/abdullahboshir/t-shirt-react-router>
2022-06-08 15:15:16 +0600

    complete header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './header.css'
 
 const Header = () => {
+    const {user, handleSingOut} = useFirebase();
     return (
         <div className='header'>
-           <nav>
-           <Link to="/">Home</Link>
-            <Link to="login">Login</Link>
-           </nav>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/register">Register</Link>
+                <span style={{paddingRight: "15px"}}>{user?.displayName && user.displayName}</span>
+                {user?.uid? <button onClick={handleSingOut}>Sign Out</button> :
+                    <Link to="/login">Login</Link>}
+            </nav>
         </div>
     );
 };
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {singINWithGoogle} = useFirebase()
     return (
         <div>
             <h3>Please Login!!</h3>
+            <button onClick={singINWithGoogle}>Google Sign In</button>
+            <form>
+            <br />
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" placeholder='Password' />
+                <br />
+                <input type="submit" value='Login' />
+            </form>
         </div>
     );
 };
